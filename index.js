955f58a0bfe225a716836c2e5378e5e01ef4ccae
Anshul Patel <91446597+cosmoverse2000@users.noreply.github.com>
2022-07-21 18:35:47 +0530

    changed the Sound links , fixed sound path

--- index.js
+++ index.js
@@ -21,25 +21,25 @@ document.addEventListener("keydown", function (event) {
 });
 function makeSound(k) {
   if (k === "w") {
-    var tom1 = new Audio("/sounds/tom-1.mp3");
+    var tom1 = new Audio("/DrumKit/sounds/tom-1.mp3");
     tom1.play();
   } else if (k === "a") {
-    var tom2 = new Audio("/sounds/tom-2.mp3");
+    var tom2 = new Audio("/DrumKit/sounds/tom-2.mp3");
     tom2.play();
   } else if (k === "s") {
-    var tom3 = new Audio("/sounds/tom-3.mp3");
+    var tom3 = new Audio("/DrumKit/sounds/tom-3.mp3");
     tom3.play();
   } else if (k === "d") {
-    var tom4 = new Audio("/sounds/tom-4.mp3");
+    var tom4 = new Audio("/DrumKit/sounds/tom-4.mp3");
     tom4.play();
   } else if (k === "j") {
-    var tom5 = new Audio("/sounds/crash.mp3");
+    var tom5 = new Audio("/DrumKit/sounds/crash.mp3");
     tom5.play();
   } else if (k === "k") {
-    var tom6 = new Audio("/sounds/kick-bass.mp3");
+    var tom6 = new Audio("/DrumKit/sounds/kick-bass.mp3");
     tom6.play();
   } else if (k === "l") {
-    var tom7 = new Audio("/sounds/snare.mp3");
+    var tom7 = new Audio("/DrumKit/sounds/snare.mp3");
     tom7.play();
   } else {
     console.log("invalid Key");
